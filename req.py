d312123018f55269476fff660cc452629062016d
Praveen Dias <praveendias1180@gmail.com>
2022-05-15 01:09:36 +0530

    Testing post with httpbin

--- req.py
+++ req.py
@@ -5,3 +5,12 @@ r = requests.get("https://httpbin.org/get")
 print(r.status_code)
 print(r.url)
 print(r.text)
+
+print("----------------------------")
+
+data = {'somekey': 'somevalue'}
+r = requests.post("https://httpbin.org/post", data = data)
+
+print(r.status_code)
+print(r.url)
+print(r.text)
