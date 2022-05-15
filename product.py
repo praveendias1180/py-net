4a9598137e041a7350094f294a6953d9c9f4df7e
Praveen Dias <praveendias1180@gmail.com>
2022-05-15 09:51:39 +0530

    POST a new product

---
+++ product.py
@@ -0,0 +1,19 @@
+import requests
+
+product = {
+        "productName":"Araliya Basmathi Rice",
+        "description":"White Basmathi Rice imported from Pakistan. High-quality rice with extra fragrance. Organically grown.",
+        "category":"Rice",
+        "brand":"CIC",
+        "expiredDate":"2023.05.04",
+        "manufacturedDate":"2022.02.20",
+        "batchNumber":324567,
+        "unitPrice":1020,
+        "quantity":200,
+        "createdDate":"2022.02.24"
+}
+
+BASE_URL = 'http://host1.open.uom.lk:8080'
+response = requests.post(f"{BASE_URL}/api/products",json=product)
+
+print(response.status_code)
