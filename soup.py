90c20d4a392e9c6ceb681322443cc7ee0b1e1db4
Praveen Dias <praveendias1180@gmail.com>
2022-05-15 08:16:35 +0530

    improved scraping

--- soup.py
+++ soup.py
@@ -6,6 +6,6 @@ url = 'https://www.google.com/search?q=Python'
 html = requests.get(url).content
 soup = BeautifulSoup(html, 'html.parser')
 
-tags = soup('a')
+tags = soup('h3')
 for tag in tags:
-    print(tag.get('href'))
+    print(tag.find('div').text)
